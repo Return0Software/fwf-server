75a659eea93ecb4298a76d4caab79d3551406af5
Tristan Partin <tristan.partin@gmail.com>
2018-12-15 02:36:46 -0600

    Add neo4j configuration variables

--- src/main/java/com/github/return0software/fwf/AppConfiguration.java
+++ src/main/java/com/github/return0software/fwf/AppConfiguration.java
@@ -1,6 +1,35 @@
 package com.github.return0software.fwf;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import io.dropwizard.Configuration;
 
 public final class AppConfiguration extends Configuration {
+	@JsonProperty
+	private String graphHost;
+
+	@JsonProperty
+	private Integer graphPort;
+
+	@JsonProperty
+	private String graphUsername;
+
+	@JsonProperty
+	private String graphPassword;
+
+	public String getHost() {
+		return this.graphHost;
+	}
+
+	public int getPort() {
+		return this.graphPort;
+	}
+
+	public String getUsername() {
+		return this.graphUsername;
+	}
+
+	public String getPassword() {
+		return this.graphPassword;
+	}
 }
