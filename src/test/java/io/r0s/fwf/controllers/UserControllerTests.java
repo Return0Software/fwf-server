f377edbdc5119d205f280576a14af22311f24918
Tristan Partin <tristan.partin@gmail.com>
2019-01-26 14:03:25 -0600

    Setup testing style

--- src/test/java/io/r0s/fwf/controllers/UserControllerTests.java
+++ src/test/java/io/r0s/fwf/controllers/UserControllerTests.java
@@ -1,13 +1,17 @@
-package io.r0s.fwf;
+package io.r0s.fwf.controllers;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public final class AppTests {
+public final class UserControllerTests {
+	@Autowired
+	private UserController userController;
+
 	@Test
 	public void example() {
 	}
