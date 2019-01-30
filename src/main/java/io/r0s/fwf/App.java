183f8a24f62ee6ecb9e22382fd519e2b487e06aa
Tristan Partin <tristan.partin@gmail.com>
2019-01-29 23:07:18 -0600

    Pass path for neo4j repository package

--- src/main/java/io/r0s/fwf/App.java
+++ src/main/java/io/r0s/fwf/App.java
@@ -2,11 +2,13 @@ package io.r0s.fwf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @SpringBootApplication
 @EnableAdminServer
+@EnableNeo4jRepositories("io.r0s.fwf.repositories")
 public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
