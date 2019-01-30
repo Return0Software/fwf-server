d73191685ab195ed780dd79a62a2014bcb268040
Tristan Partin <tristan.partin@gmail.com>
2019-01-29 23:23:01 -0600

    Fix tests due to erronious constructor

--- src/test/java/io/r0s/fwf/controllers/PlaylistControllerTests.java
+++ src/test/java/io/r0s/fwf/controllers/PlaylistControllerTests.java
@@ -2,17 +2,15 @@ package io.r0s.fwf.controllers;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public final class PlaylistControllerTests {
-	private final PlaylistController playlistController;
-
-	public PlaylistControllerTests(final PlaylistController playlistController) {
-		this.playlistController = playlistController;
-	}
+	@Autowired
+	private PlaylistController playlistController;
 
 	@Test
 	public void example() {
